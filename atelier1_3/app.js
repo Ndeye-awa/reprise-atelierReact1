6e36447b87c3f3213bdcfcf30635ff7d22807915
Ndèye Awa Goumbala Sambe <goumbalasambe@gmail.com>
2024-08-01 11:55:14 +0000

    Atelier 1_3

--- atelier1_3/app.js
+++ atelier1_3/app.js
@@ -3,21 +3,22 @@ class Todos extends React.Component {
     super(props);
     this.state = {
       input: "",
-      todos: [],
+      todos: []
     };
   }
   render() {
     return (
       <div className="container mt-4">
-        <h1>To-do List</h1>
+        <h1>To-Do List</h1>
         <p>{this.state.input}</p>
         <div className="input-group mb-3">
           <input
             type="text"
             value={this.state.input}
-            onChange={(e) => this.setState({ input: e.target.value })}
+            onChange={(e) => this.setState({ input: e.target.value })
+            }
             className="form-control"
-            placholder="Ajouter une tâche"
+            placeholder="Ajouter une Tâche"
           />
           <button
             className="btn btn-primary"
